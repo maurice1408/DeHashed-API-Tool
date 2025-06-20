9cb3667c657ab06ce5d4f33e31d78390ea5b5db5
Maurice Hickey <maurice1408@gmail.com>
2025-06-20 16:41:42 +0100

    Version delivered to VD

--- README.md
+++ README.md
@@ -1,151 +1,70 @@
-# DeHashed-API-Tool
-A command-line tool to query the Dehashed API. Easily search for various parameters like usernames, emails, hashed passwords, IP addresses, and more.
+# DeHashed Client
 
-## Features
-- Search the Dehashed API using multiple parameters.
-- Output unique results to a CSV file.
-- Fetch unique password results.
-- Silent output mode for minimal console output.
+A small Marimo Notebook that acts as a browser based client for
+DeHashed.
 
-## Installation
-### Prerequisites
-1. Ensure you have Python 3.x installed.
-2. A valid DeHashed account, API key, and paid API credits are required.
-3. Ensure pip or pipx is installed (pipx recommended)
 
-### Pipx install
-```bash
-pipx install git+https://github.com/hmaverickadams/DeHashed-API-Tool
+In an attempt to avoid Python "dependency hell" the client runs
+through the 'uv` package manager.
+
+Install `uv` on your mac using the command:
+
+```
+curl -LsSf https://astral.sh/uv/install.sh | sh
 ```
 
-> **NOTE**: You can also install with regular pip using `pip install git+https://github.com/hmaverickadams/DeHashed-API-Tool` but pipx is recommended to ensure you don't break python dependencies to other packages.
+or if you are more comfortable using Homebrew
 
-### Add API Credential
-The API email and key input for each request or stored in the config.txt file (in cleartext)
 
-**Run without storing keys**
-```bash
-dehashapitool -u username --key
 ```
-> **NOTE**: You will be prompted for the API key
-
-**Store Keys in config.txt file (Key prompted)**
-```bash
-dat --store-creds
+brew install uv
 ```
 
-> **NOTE**: `dat` can be used as the command instead of `dehashapitool` to save some keystrokes, but either one will run the script.
+if `uv` installs successfully then the command uv --version
+should give you something along the lines of...
 
-**Run with stored keys**
-```bash
-dat -u username
+```
+uv --version 
+uv 0.7.12 (dc3fd4647 2025-06-06)
 ```
 
-## Usage
-### Running the tool:
-`dehashapitool -h`
+Create a new directory e.g. `mkdir deh` cd to that directory `cd
+deh` and copy the attached `deh.py` file into that directory.
 
-### Options:
+From that directory issue the command
+
 ```
-usage: dehashapitool [-h] [-a ADDRESS] [-e EMAIL] [-H HASHED_PASSWORD] [-i IP_ADDRESS] [-n NAME] [-p PASSWORD] [-P PHONE] [-u USERNAME] [-v VIN] [-d DOMAIN] [-o OUTPUT] [-oS OUTPUT_SILENTLY]
-                     [-s SIZE] [--only-passwords] [--regex] [--key [DEHASHED_KEY]] [--store-key]
-
-Query the Dehashed API
-
-options:
-  -h, --help            show this help message and exit
-  -a ADDRESS, --address ADDRESS
-                        Specify the address
-  -e EMAIL, --email EMAIL
-                        Specify the email
-  -H HASHED_PASSWORD, --hashed_password HASHED_PASSWORD
-                        Specify the hashed password
-  -i IP_ADDRESS, --ip_address IP_ADDRESS
-                        Specify the IP address
-  -n NAME, --name NAME  Specify the name
-  -p PASSWORD, --password PASSWORD
-                        Specify the password
-  -P PHONE, --phone PHONE
-                        Specify the phone number
-  -u USERNAME, --username USERNAME
-                        Specify the username
-  -v VIN, --vin VIN     Specify the VIN
-  -d DOMAIN, --domain DOMAIN
-                        Specify the domain
-  -o OUTPUT, --output OUTPUT
-                        Outputs to CSV. A file name is required.
-  -oS OUTPUT_SILENTLY, --output_silently OUTPUT_SILENTLY
-                        Outputs to CSV silently. A file name is required.
-  -s SIZE, --size SIZE  Specify the size, between 1 and 10000
-  --only-passwords      Return only passwords
-  --regex               Use regex search instead of string (seems to be broken as of May 2025)
-
-API Arguments:
-  Arguments related to Dehashed API credentials
-
-  --key [DEHASHED_KEY], --dehashed-key [DEHASHED_KEY]
-                        Dehashed API key (overrides config.txt value)
-  --store-key           Stored the Dehashed API key in cleartext in the config.txt file (overrides previous config.txt values)
-
-Usage examples:
-  dat --key <API_KEY> --store-creds
-  dehashapitool -u username
-  dehashapitool -e email@example.com --output results.csv
-  dat -d example.com --only-passwords
-  dat -i 192.168.0.1 -s 100 -de jdoe@example.com --key
+uvx marimo run deh.py
 ```
 
-### Basic Usage:
-`dehashapitool -u username`
+You should get the prompt...
 
-The above will return all results for the queried username.
-
-### Output Unique Results to a CSV:
-`dehashapitool -e email@example.com -o results.csv`
-
-The above will return all results for the queried email address and store it to a csv.
-
-### Output Passwords Only:
-`dehashapitool -d example.com --only-passwords`
-
-The above will return all passwords for the queried domain, sorted alphabetically by the field query.  Example results:
 ```
-email: bob@example.com, password: Bobert123!
-email: mike@example.com, password: 2813308004
-You have 40 API credits remaining
+This notebook has inlined package dependencies.
+Run in a sandboxed venv containing this notebook's dependencies? [Y/n]:
 ```
 
-### Silent Output to CSV:
-`dehashapitool -d example.com --only-passwords -oS results.csv`
+answer `Y` to that.
 
-The above will return all passwords for the queried domain, sorted alphabetically by the field query and store it to a csv while not outputting to the screen.
+That should open up the search tool in your browser. 
 
-### Multiple Search Parameters:
-`dehashapitool -d example.com -p password`
+From there you should be able to enter a search term in the Enter
+search text input field then click on the **Go!** button to execute
+the search. The results should appear in the table below.  
 
-**Note: at the time of development, this search is considered an OR statement as it does not seem possible to use an AND query in the current API.**
+The 2 icons at the bottom left of the results table let you:
 
-## Advanced Usage
-### Wildcards
-`dehashapitool -n -e '"examp?e"' -s 10`
+1. Search the results
+2. Toggle the row viewer that will appear to the right of the
+   table
 
-`dehashapitool -n -e '"examp*e"' -s 10`
+Selecting rows (checkbox to let of row) will populate rows in
+the table below the main results table. So selecting rows can be
+used to keep rows of interest.
 
-The above will return 10 results utilizing the wildcard
+Either table can be downloaded or coppied to clipboard using the
+`Download` at the bottom right of either table.
 
-> **Note**: As of May 2025, the wildcard search using "*" seems to be broken in the V2 API.
-
-## Contributions
-Contributions are always welcome! Please open an issue or submit a pull request.
-
-Special thanks:<br/>
-Duncan Woosley (d-woosley) - Contributions to v1 to v2 API upgrades
-
-## Copyright
-DeHashed API Tool by Heath Adams Copyright (C) 2025 TCM Security, Inc.
-
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/
+Hopefully you will not hit any issues that highlight dependencies
+on my environment!  Let me know how it goes and / or any
+questions or suggestions!
---
+++ deh.py
@@ -0,0 +1,383 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "requests==2.31.0",
+#   "pydantic==2.10.5",
+#   "polars==1.18.0",
+#   "marimo",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.14.0"
+app = marimo.App(width="medium", app_title="DeHashed")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import requests
+
+    import hashlib
+    import polars as pl
+    from pydantic import BaseModel, Field
+    from typing import Optional,List
+
+    import asyncio
+
+    # api_key = "wAHd3cTDijZItfXV2wYJLNYypfFgHrjedUBvpK6owbUVLhqWkPDOqnI="
+    return BaseModel, Field, hashlib, mo, pl, requests
+
+
+@app.cell
+def _(conv_to_str, mo, pl):
+    def disply_res(results):
+
+        df = pl.DataFrame(results)
+        df_str = conv_to_str(df)
+        y = mo.ui.table(df_str, selection="multi")
+        # s = y.value
+
+        return y
+
+    return (disply_res,)
+
+
+@app.cell
+def _(api_key, requests):
+    async def v2_search(query:str,page:int,size:int,wildcard:bool,regex:bool,de_dupe:bool) -> dict:
+
+        qry = {"query": query,
+               "page": page,
+               "size": size,
+               "wildcard": wildcard,
+               "regex": regex,
+               "de_dupe": de_dupe}
+
+        # print(qry)
+
+        res = requests.post("https://api.dehashed.com/v2/search", 
+                            json=qry,
+                            headers={
+                                "Content-Type": "application/json",
+                                "DeHashed-Api-Key": api_key,
+                            })
+        return res.json()
+    return (v2_search,)
+
+
+@app.cell
+def _(api_key, hashlib, requests):
+    def get_sha256(password:str) -> str:
+        return hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    def v2_search_password(password:str)-> dict:
+        sha256_hash = get_sha256(password)
+        res = requests.post("https://api.dehashed.com/v2/search-password", json={
+            "sha256_hashed_password": sha256_hash,
+        }, headers={
+            "Content-Type": "application/json",
+            "DeHashed-Api-Key": api_key,
+        })
+        return res.json()
+    return
+
+
+@app.cell
+def _(mo):
+    def show_resp(rsp, srch):
+
+        # print("In function show_resp")
+        # print(type(rsp))
+        # print(rsp.keys())
+
+        if "error" in rsp.keys():
+            return mo.callout(f"""
+            Error: {rsp["error"]}
+            """, kind="warn")
+        else:
+            # return mo.md(f"""
+            #    The search against **{srch}** returned 
+            #    **{len(rsp["entries"])}** entries in **{rsp["took"]}**,
+            #    you have a remaining balance of **{rsp["balance"]}** API calls.
+            #    """
+            srch_term = mo.stat(label="Search Term", value=srch)
+            srch_ent = mo.stat(label="Entries Returned", value=len(rsp["entries"]))
+            srch_time = mo.stat(label="Execution Time", value=rsp["took"])
+            # srch_bal = mo.stat(label="API Balance", value=rsp["balance"])
+
+            return mo.hstack(items=[srch_term, srch_ent, srch_time])
+
+    return (show_resp,)
+
+
+@app.cell
+def _(BaseModel, Field):
+    class Dehashed(BaseModel):
+        id: str
+        email: list = Field(default_factory=list)
+        ip_address: list = Field(default_factory=list)
+        username: list = Field(default_factory=list)
+        password: list = Field(default_factory=list)
+        hashed_password: list = Field(default_factory=list)
+        name: list = Field(default_factory=list)
+        dob: list = Field(default_factory=list)
+        license_plate: list = Field(default_factory=list)
+        address: list = Field(default_factory=list)
+        phone: list = Field(default_factory=list)
+        company: list = Field(default_factory=list)
+        url: list = Field(default_factory=list)
+        social: list = Field(default_factory=list)
+        cryptocurrency_address: list = Field(default_factory=list)
+        database_name: str
+    return (Dehashed,)
+
+
+@app.cell
+def _(pl):
+    def conv_to_str(df):
+        """DeHashed returns data elemets as Lists.
+
+        This function converts the Lists to Strings
+        """
+
+        join_str = " / "
+
+        return df.with_columns(
+            pl.col("email").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("ip_address").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("username").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("password").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("hashed_password").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("name").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("dob").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("license_plate").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("address").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("phone").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("company").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("url").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("social").cast(pl.List(pl.String)).list.join(join_str),
+            pl.col("cryptocurrency_address").cast(pl.List(pl.String)).list.join(join_str)
+        )
+
+    return (conv_to_str,)
+
+
+@app.cell
+def _(mo):
+    mo.md(
+        r"""
+    # DeHashed Search
+
+    ## Introduction
+
+    This Marimo notebook will allow you to enter a search phrase against DeHashed. 
+
+    The results will be returned in a Polars DataFrame. The Marimo UI on the DataFrame will allow you to, search, sort and download the returned data.
+
+    /// note | Note 
+
+    _not_ all elements in each row will be populated, it will depend on the search results.
+    ///
+    """
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    key=mo.ui.text(label="DeHashed API Key", value="wAHd3cTDijZItfXV2wYJLNYypfFgHrjedUBvpK6owbUVLhqWkPDOqnI=")
+    key
+    return (key,)
+
+
+@app.cell
+def _(key):
+    api_key = key.value
+    return (api_key,)
+
+
+@app.cell
+def _(mo):
+    res_slider = mo.ui.slider(start=0, 
+                              stop=10000, 
+                              step=1000, 
+                              value=1000, 
+                              show_value=True, 
+                              label="Number of results to return"
+                             )
+
+    dedupe_check = mo.ui.checkbox(label="Dedupe search results", value=True)
+
+    search_type = mo.ui.radio(
+        options=["normal", "wildcard", "regexp"], value="normal", label="Choose Search Type"
+    )
+    return dedupe_check, res_slider, search_type
+
+
+@app.cell
+def _(dedupe_check, mo, res_slider, search_type):
+    mo.vstack(align='center',
+        items=[
+            res_slider,
+            dedupe_check,
+            search_type
+        ]
+    )
+    return
+
+
+@app.cell
+def _(mo):
+    input_srch = mo.ui.text(label="Search for:", full_width=True, placeholder="Enter search text")
+    srch_button = mo.ui.run_button(label="Go!", tooltip="Click to run search", full_width=True)
+    mo.vstack(
+        items=[
+            input_srch,
+            srch_button
+        ],align="stretch"
+    )
+    return input_srch, srch_button
+
+
+@app.cell
+def _(input_srch, srch_button):
+    if srch_button.value: 
+        srch = input_srch.value
+    return (srch,)
+
+
+@app.cell
+def _(search_type):
+    rexexp = False
+    wildcard = False
+
+    match search_type.value:
+        case "normal":
+            regexp=False
+            wildcard=False
+        case "wildcard":
+            regexp=False
+            wildcard=True
+        case "regexp":
+            regexp=True
+            wildcard=False
+
+    return regexp, wildcard
+
+
+@app.cell
+async def _(
+    dedupe_check,
+    mo,
+    regexp,
+    res_slider,
+    srch,
+    srch_button,
+    v2_search,
+    wildcard,
+):
+    # response = v2_search_password(pwd.value)
+    _output = None
+
+    response = {}
+
+    if srch_button.value:
+        with mo.status.spinner(title="searching...") as _spinner:
+            response = await v2_search(srch, 1, res_slider.value, wildcard, regexp, dedupe_check.value)
+            _spinner.update("Done")
+
+    _output
+    return (response,)
+
+
+@app.cell
+def _(mo, response, show_resp, srch, srch_button):
+    _op = None
+
+    if srch_button.value:
+        _stat = show_resp(response, srch)
+        if _bal := response.get("balance"):
+            if _bal < 100:
+                _callout = mo.callout(f"Low API credit balance: {_bal}", kind="warn")
+            else:
+                _callout = mo.callout(F"Balance is {_bal} API credits", kind="info")
+        else:
+            _callout = mo.callout("no balance returned", kind="warn")
+
+        _op = mo.vstack(items=[
+                        _stat,
+                        _callout
+                        ]
+                     )
+    _op
+    return
+
+
+@app.cell
+def _(Dehashed, response, srch_button):
+    l = []
+    if srch_button.value:
+        if "entries" in response:
+            for i in range(len(response["entries"])):
+                m = Dehashed.model_validate(response["entries"][i])
+                l.append(m)
+    return (l,)
+
+
+@app.cell
+def _(l, pl):
+    df = pl.DataFrame(l)
+    return (df,)
+
+
+@app.cell(disabled=True)
+def _(df, pl, srch_button):
+    tbl = None
+    if srch_button.value:
+        df_str = df.with_columns(
+                pl.col("email").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("ip_address").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("username").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("password").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("hashed_password").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("name").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("dob").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("license_plate").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("address").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("phone").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("company").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("url").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("social").cast(pl.List(pl.String)).list.join(", "),
+                pl.col("cryptocurrency_address").cast(pl.List(pl.String)).list.join(", ")
+            )
+
+        # tbl = mo.ui.table(df_str, selection="multi")
+
+    tbl
+    return
+
+
+@app.cell
+def _(disply_res, l, srch_button):
+    r = None
+
+    if srch_button.value:
+        r = disply_res(l)
+
+    r
+    return (r,)
+
+
+@app.cell
+def _(l, r):
+    v = None
+    if len(l):
+        v = r.value
+    v
+    return
+
+
+if __name__ == "__main__":
+    app.run()
